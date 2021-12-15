cdf7a435f228710e5c850a4763c4962fb73bada6
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2021-12-15 04:04:25 -0600

    beginning of code - addition of the departmentname, rolename, rolesalary, rolesalary

--- assets/index.js
+++ assets/index.js
@@ -9,24 +9,54 @@ const inquirer = require('inquirer');
 
 //question prompts
 const questions = [
-    // {
-    //     type: 'list',
-    //     name: 'action',
-    //     message: 'What would you like to do?',
-    //     choices: ['View all employees', 'Add employee', 'Update employee role', 'View all roles', 'Add role', 'View all departments', 'Add department', 'Quit']
-    // },
+    // WHEN I start the application
+    // THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+    {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['View all employees', 'Add employee', 'Update employee role', 'View all roles', 'Add role', 'View all departments', 'Add department', 'Quit']
+    },
+    //add department
     {
         type: 'input',
-        name: 'addEmployee',
+        name: 'departmentName',
+        message: 'What is the a name of the department?',
+        when: (questions) => questions.action === 'Add department',
+    },
+    {
+        type: 'input',
+        name: 'roleName',
+        message: 'What is the a name of the role',
+        when: (questions) => questions.action === 'Add roles',
+    },
+    {
+        type: 'input',
+        name: 'roleSalary',
+        message: 'What is the a salary of the role?',
+        when: (questions) => questions.action === 'Add roles',
+    },
+    {
+        type: 'input',
+        name: 'roleDept',
+        message: 'Which department does the role belong to?',
+        when: (questions) => questions.action === 'Add roles',
+    },
+    {
+        type: 'input',
+        name: 'firstName',
         message: "What is the employee's first name?"
+
     }
 ]
 
 const init = async() => {
     const answer = await inquirer.prompt(questions)
-    const employee = answer.addEmployee
-    // console.log(employee.addEmployee) 
-    console.log(employee)
+    const employee = answer.firstName
+
+    console.log(answer) 
+    
+    // console.log(employee)
     return employee
 }
 // init();
--- helpers/utils.js
+++ helpers/utils.js
@@ -3,10 +3,11 @@
 
 
 //test query
-const testQuery = 'INSERT INTO employee (first_name) VALUES ("'
+// const testQuery = 'INSERT INTO employee (first_name) VALUES ("'
+const testQuery = 'INSERT INTO employee (first_name) VALUES ("?")'
 // console.log(testQuery)
 //ending paratheses
 const endParan = '")'
 //select * f
-const selectEmp= `SELECT * from employee`
+const selectEmp= `SELECT * from ?`
 module.exports = { testQuery, selectEmp, endParan }

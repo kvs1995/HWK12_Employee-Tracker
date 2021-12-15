0bc715078381ae19ba621adb265d8f6a693cd683
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2021-12-15 03:10:01 -0600

    much more than prompt questions but related

---
+++ helpers/utils.js
@@ -0,0 +1,12 @@
+
+// const init = require('../assets/index')
+
+
+//test query
+const testQuery = 'INSERT INTO employee (first_name) VALUES ("'
+// console.log(testQuery)
+//ending paratheses
+const endParan = '")'
+//select * f
+const selectEmp= `SELECT * from employee`
+module.exports = { testQuery, selectEmp, endParan }
